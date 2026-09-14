db15a8236cb6afc9b9e952737f91978054c80adc
zhengzhunlun19881225 <fj4wsdbbw5@privaterelay.appleid.com>
2026-09-14 18:13:52 +0800

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/VR-Panorama-Platform/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
